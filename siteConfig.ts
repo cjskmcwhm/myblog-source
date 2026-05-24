cafdcb1b5b4430bdbb62e9f2858e9aa9c4b85b02
cjskmcwhm <seamushampton5077955@gmail.com>
2026-05-24 12:05:50 +0800

    add gitalk config

--- siteConfig.ts
+++ siteConfig.ts
@@ -2,12 +2,12 @@
 
 export const siteConfig = {
   // 1. 网站标题与博主信息
-  title: "XingHuiSama の 宝藏之地",
+  title: "飘落澪 の 宝藏之地",
   faviconUrl: "https://bu.dusays.com/2026/03/24/69c1e38ac1846.jpg",
-  authorName: "XingHuiSama",
-  bio: "在代码、学术与分子动力学模拟间穿梭的普通人。近期正埋头于 GROMACS 模拟研究与神经网络计算。",
+  authorName: "飘落澪",
+  bio: "喜欢下载研究部署别人的开源软件然后测试。",
 
-  navTitle: "XingHuiSama",
+  navTitle: "飘落澪",
 
   // 👇 【新增】导航栏中间的那个后缀/分隔符（默认是 の）
   navSuffix: "の",
